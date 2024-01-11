fdd148c8c1102c50993b17cb65962b7d78fd7567
Adam Klement <ADmin0908@seznam.cz>
2024-01-11 20:08:27 +0100

    Update primary green color and navbar width, add border radius and background color to navbar, adjust font size and padding for nav links, update hover styles for nav links and buttons, update header-blog-post height and margin, add overflow property to header-blog-post, adjust height of active nav-links, update media queries for smaller screens.
    Toggle theme functionality added to navbar.js.

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,3 +6,30 @@ function toggle_navbar() {
         navbar_links[i].classList.toggle("active");
     }
 }
+
+var theme_toggler = document.getElementById("theme-toggler");
+
+function toggle_theme() {
+    document.body.classList.toggle("dark-theme");
+    if (theme_toggler.innerHTML == '<i class="ph-bold ph-moon"></i>') {
+        theme_toggler.innerHTML = '<i class="ph-bold ph-sun"></i>';
+    } else {
+        theme_toggler.innerHTML = '<i class="ph-bold ph-moon"></i>';
+    }
+
+    if (document.body.classList.contains("dark-theme")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+
+var theme = localStorage.getItem("theme");
+if (theme == "dark") {
+    document.body.classList.add("dark-theme");
+    theme_toggler.innerHTML = '<i class="ph-bold ph-moon"></i>';
+} else {
+    document.body.classList.remove("dark-theme");
+    theme_toggler.innerHTML = '<i class="ph-bold ph-sun"></i>';
+}
